489e8ba8e261c5c26da558db7acac49a656eb3fb
kamil79123 <kamil79123@gmail.com>
2024-12-05 16:19:20 +0100

    Pierwsza edycja

---
+++ assets/js/newsapi.js
@@ -0,0 +1,65 @@
+async function fetchNews(search, language, prioritydomain, search_fields, country, sort) {
+    const loadingSpinner = document.getElementById('loader');
+    loadingSpinner.style.display = 'block';  // Pokaż animację wczytywania
+    const container = document.getElementById('row-3');  // Element HTML, gdzie będą wyświetlane artykuły
+    $('#found').html('');
+    $(container).html('');
+    let i = 0;
+    let params = {
+        query: search,
+        language: language,
+        prioritydomain: prioritydomain,
+        country: country
+    };
+    // Tworzenie zapytania do serwera PHP
+    let query = new URLSearchParams(params).toString();
+    let apiUrl;
+    if ($('#search-titles').is(':checked')) {
+        console.log('titles dziala');
+        apiUrl = "../../proxy-title.php?" + query;
+    }
+    else {
+        console.log('bez titles');
+        apiUrl = "../../proxy.php?" + query;
+    }
+    let str = '';  // Zmienna przechowująca wygenerowany HTML
+    try {
+        // Pobranie danych z serwera PHP
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            loadingSpinner.style.display = 'none';
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // Przetwarzanie odpowiedzi JSON
+        const data = await response.json();
+        let articles = data.results;
+        if (!articles) {
+            loadingSpinner.style.display = 'none';
+            throw new Error('Brak artykułów w odpowiedzi z API');
+        }
+        if (sort == 'new') {
+            loadingSpinner.style.display = 'none';
+            articles.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+            console.log("nowszych");
+        }
+        else {
+            loadingSpinner.style.display = 'none';
+            articles.sort((a, b) => new Date(a.pubDate) - new Date(b.pubDate));
+            console.log("starszych");
+        }
+        // Iteracja po artykułach i generowanie HTML
+        for (let article of articles) {
+            let description = article.description || 'Więcej treści po kliknięciu w link prowadzący do artykułu';
+            let articleHTML = `<div class="mx-auto col-xs-10 col-sm-5 offset-md-0 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0 anim flex my-3 text-center"><div class="card"><img src="${article.image_url}" class="card-img-top" alt="..."><span class="category">${article.category}</span><img class="icon" src="${article.source_icon}"><span class="country">${article.country}, ${article.source_url}</span><div class="card-body"><h5 class="card-title">${article.title}</h5><p class="card-text">${description}</p><p class="date mb-5">${article.pubDate}</p><div style="margin-top: 5vh"><a href="${article.link}" target="_blank" class="position-absolute bottom-0 translate-middle article-btn btn btn-primary">Czytaj dalej</a></div></div></div></div>`;
+            str += articleHTML;
+            i++;
+        }
+        // Dodanie wygenerowanego HTML do kontenera
+        container.innerHTML = str;
+        $('#found').html('Znaleziono ' + i + ' artykułów');
+    } catch (error) {
+        // Obsługa błędów, np. problem z pobraniem danych
+        console.error("Błąd podczas pobierania lub przetwarzania danych:", error);
+        container.innerHTML = "<p>Nie udało się załadować artykułów. Spróbuj ponownie później.</p>";
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    var sort = 'new';
+    $('#new').click(function () {
+        if (sort != 'new') {
+            $('#old').toggleClass('active');
+            $('#new').toggleClass('active');
+            sort = 'new';
+            szukaj(sort);
+        }
+    });
+    $('#old').click(function () {
+        if (sort != 'old') {
+            $('#new').toggleClass('active');
+            $('#old').toggleClass('active');
+            sort = 'old';
+            szukaj(sort);
+        }
+    });
+    $('#search-prioritydomain').click(function () {
+        szukaj(sort);
+    });
+
+    $('#search-titles').click(function () { // Wychwytujemy kliknięcie przycisku
+        szukaj(sort);
+    });
+    $("#search-submit").click(function () { 
+        szukaj(sort);
+    });
+});
+
+function szukaj(sort) {
+    let search = $('#search-keyword').val(); // Zmienna z zapytaniem
+    let search_fields = 'title,main_text'; // Zmienna wskazująca, gdzie szukamy w artykule
+    if ($('#search-titles').is(':checked')) {
+        search_fields = 'title'; // Jeśli checkbox zaznaczony to szukamy tylko w tytule
+    }
+    let prioritydomain = 'low';
+    if ($('#search-prioritydomain').is(':checked')) {
+        prioritydomain = 'medium';
+    }
+    const selectLanguage = document.getElementById('language');
+    const selectedLanguage = Array.from(selectLanguage.selectedOptions);
+    const language = selectedLanguage.map(option => option.value).join(',');
+    const selectCountry = document.getElementById('country');
+    const selectedCountry = Array.from(selectCountry.selectedOptions);
+    let country = selectedCountry.map(option => option.value).join(',');
+    if (search != '' && selectedLanguage != '' && selectedCountry != '') {
+        console.log("selectedLanguage: " + selectedLanguage);
+        console.log("selectedCountry" + selectedCountry);
+        fetchNews(search, language, prioritydomain, search_fields, country, sort);
+    }
+    else {
+        $('#found').html('<p style="color: red; font-weight: bold">Proszę uzupełnić formularz</p>');
+    }
+}
